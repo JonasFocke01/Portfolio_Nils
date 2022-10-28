84f31a5d30a443b58c3995226ea1912a0f76f4b9
jonas_focke <jonas.focke@beresa.de>
2022-10-28 17:09:28 +0200

    implement calendar component

---
+++ src/lib/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { InlineCalendar } from 'svelte-calendar';
+	const theme = {
+		calendar: {
+			width: '600px',
+			shadow: '0px 0px 5px rgba(0, 0, 0, 0.25)'
+		}
+	};
+</script>
+
+<div class="w-full flex flex-col text-center">
+	<p class="text-2xl">Du willst mich buchen?</p>
+	<InlineCalendar {theme} />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<div class="w-full text-9xl flex flex-row justify-center">
-	<p>DJ SERVICE NILS FOCKE</p>
+<script lang="ts">
+	import Contact from '$lib/contact/+page.svelte';
+</script>
+
+<div class="w-full flex flex-row justify-center">
+	<div class="flex flex-col">
+		<p>DJ SERVICE NILS FOCKE</p>
+		<Contact />
+	</div>
 </div>

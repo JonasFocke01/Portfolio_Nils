4453160815f79296a8b29f52dba0cb3483ad79b8
jonas_focke <jonas.focke@beresa.de>
2022-10-28 15:29:48 +0200

    refactor: overhaul complete site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,5 @@
 <script>
 	import '../app.css';
-	import Track from '$lib/components/Track/Track.svelte';
-	import Controller from '$lib/components/Controller/Controller.svelte';
-	import { onMount } from 'svelte';
-	import { leftTrack, rightTrack } from '$lib/stores/tracks';
-
-	onMount(() => {
-		leftTrack.set({
-			title: 'demo',
-			path: 'demo.mp3',
-			level: 0.5,
-			playing: false
-		});
-
-		rightTrack.set({
-			title: 'demo_2',
-			path: 'demo_2.mp3',
-			level: 0.5,
-			playing: false
-		});
-	});
 </script>
 
-<!-- mixing Controller  -->
-<div class="absolute flex flex-row justify-center w-full max-w-1280 h-screen z-10">
-	<div class="flex flex-col justify-end h-screen w-full ">
-		<Controller />
-	</div>
-</div>
-
-<div class="w-full flex flex-row min-h-screen bg-black">
-	<!-- left side track -->
-	<div class="w-1/6">
-		<Track side="left" />
-	</div>
-
-	<div class="w-2/3 ">
-		<slot />
-	</div>
-
-	<!-- right side track -->
-	<div class="w-1/6">
-		<Track side="right" />
-	</div>
-</div>
+<slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/@jonas_focke/**/*.{html,js,svelte,ts}'
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		colors: {
-			primary: '#ed5e93',
-			secondary: '#fb4f93',
-			surface: '#bab7af',
-			accent: '#d3fbd8',
-			success: '#4dea3f',
-			warning: '#f4bb50',
-			error: '#f74838',
-			info: '#62acfc',
-			text: '#ffffff',
-			black: '#000000'
-		},
-		plugins: []
-	}
+		extend: {}
+	},
+	plugins: []
 };

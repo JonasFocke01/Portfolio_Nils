cd3c192e2885f1d439ca653d4fedda8279104fc5
Jonas <jonas-focke@mailbox.org>
2023-02-27 21:08:43 +0100

    feat: 🎉 import logo

--- src/lib/lander/+page.svelte
+++ src/lib/lander/+page.svelte
@@ -2,17 +2,16 @@
 	import Text from '@jonas_focke/svelcon/Wrapper/Text.svelte';
 	import MediaQuery from '@jonas_focke/svelcon/Wrapper/MediaQuery.svelte';
 	import { scrollTo } from 'svelte-scrolling';
+	import Logo from '$lib/Logo/+page.svelte';
 </script>
 
 <div class="w-full h-screen flex flex-row justify-center mainBackground">
-	<div class="h-screen flex flex-col justify-center">
-		<div class="text-7xl p-4">
-			<Text text="*NILS FOCKE*" />
-		</div>
-		<MediaQuery query="desktop">
+	<div class="h-screen flex flex-col justify-center w-1/2">
+		<Logo />
+		<!-- <MediaQuery query="desktop">
 			<div class="w-full mt-4 flex flex-col lg:flex-row lg:space-x-10 justify-center">
 				<div
-					use:scrollTo={{ ref: 'contact', offset: -window.innerHeight * 0.9, duration: 1500 }}
+					use:scrollTo={{ ref: 'contact', offset: -window.innerHeight * 0.8, duration: 1500 }}
 					class="mt-4 w-full text-2xl flex flex-row justify-center bg-secondary font-bold p-2 border rounded-md"
 				>
 					<Text text="*Kontakt*" />
@@ -26,7 +25,7 @@
 					<Text text="*Live DJ Mix*" />
 				</a>
 			</div>
-		</MediaQuery>
+		</MediaQuery> -->
 	</div>
 </div>
 

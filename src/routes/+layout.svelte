e5dbbd4e9e7706010151f7c62ee4ec96fc92f7b8
Jonas <jonas-focke@mailbox.org>
2023-03-04 16:44:30 +0100

    feat: 🚧 svelte toast is now available

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import '../app.css';
+
+	const options = {
+		duration: 10000,
+		reversed: true,
+		
+	};
 </script>
+
+<SvelteToast {options} />
 
 <slot />
